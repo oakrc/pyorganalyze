ed1f46b6171766a9835781f51143fb2b11cc383e
Zhenkai Weng <oakrc@protonmail.com>
2020-10-05 10:05:27 -0700

    Create OrgData

--- package/organalyze.py
+++ package/organalyze.py
@@ -2,8 +2,7 @@
 import os
 import sys
 import argparse
-from pyorganalyze.data import process_file, create_db, close_db
-from orgparse import load
+from pyorganalyze.data import OrgData
 
 
 def parse_arguments():
@@ -36,18 +35,8 @@ def parse_arguments():
 if __name__ == '__main__':
     args = parse_arguments()
     if args.server:
-        sys.exit("Web interface is under development")
+        # TODO web interface
+        raise NotImplementedError
 
-    org_fnames = args.files or []
-    if args.dirs:
-        for d in args.dirs:
-            for root, dirs, files in os.walk(d):
-                for f in files:
-                    if f.endswith(".org"):
-                        org_fnames.append(os.path.join(root, f))
-
-    create_db()
-    for org_fname in org_fnames:
-        process_file(load(org_fname))
-
-    close_db()
+    data = OrgData()
+    data.process_files(args.files, args.dirs)
--- package/pyorganalyze/data.py
+++ package/pyorganalyze/data.py
@@ -2,66 +2,94 @@ from collections import defaultdict
 import os
 import regex
 import sqlite3
-
-tag_hierarchy = defaultdict(set)
-tag_ancestors = defaultdict(set)
+from orgparse import load
 
 
-def create_db(path=':memory:'):
-    """Create a database connection."""
-    global db
-    global db_path
-    db_path = path
-    preexisting = os.path.isfile(db_path)
-    db = sqlite3.connect(db_path)
-    cursor = db.cursor()
-    if not preexisting:
-        with open(os.path.join(
-                os.path.dirname(os.path.realpath(__file__)),
-                'database.sql')) as f:
-            cursor.executescript(f.read())
+class OrgData:
+    tag_hierarchy = defaultdict(set)
+    tag_ancestors = defaultdict(set)
 
+    def __init__(self, save_to=':memory:'):
+        self.db_path = save_to
 
-def save_db(filename):
-    """Save in-memory database"""
-    if db_path != ':memory:' and db:
-        pass
+        preexisting = os.path.isfile(db_path)
+        self.db = sqlite3.connect(db_path)
 
+        # create db if no db exists
+        if not preexisting:
+            cursor = self.db.cursor()
+            with open(os.path.join(
+                    os.path.dirname(os.path.realpath(__file__)),
+                    'database.sql')) as f:
+                cursor.executescript(f.read())
 
-def close_db():
-    """Close database connection."""
-    global db
-    global db_path
-    if db:
-        db.close()
-        db = None
-        db_path = None
+    def __del__(self):
+        """Close database connection."""
+        global db
+        global db_path
+        if db:
+            db.close()
+            db = None
+            db_path = None
 
+    def save(self, filename=None):
+        """Save in-memory database"""
+        if not db:
+            raise ValueError("No database connection.")
+        if filename and self.db_path == ':memory:':
+            # TODO
+            raise NotImplementedError
+        else:
+            self.db.commit()
 
-def build_tag_ancestors(parents=[]):
-    """Build tag_ancestors (inefficiently)."""
-    it = None
-    cur = None
-    if parents.len:
-        cur = tag_hierarchy[parents[-1]]
-        it = tag_hierarchy[cur]
-        tag_ancestors[cur].update(parents[:-1])
-    else:
-        it = tag_hierarchy.keys()
-    for tag in it:
-        build_tag_ancestors((parents + [tag]) if parents.len else [cur])
+    def build_tag_ancestors(self, parents=[]):
+        """Build tag_ancestors (inefficiently)."""
+        it = None
+        cur = None
+        if parents.len:
+            cur = self.tag_hierarchy[parents[-1]]
+            it = self.tag_hierarchy[cur]
+            self.tag_ancestors[cur].update(parents[:-1])
+        else:
+            it = self.tag_hierarchy.keys()
+        for tag in it:
+            self.build_tag_ancestors(
+                (parents + [tag]) if parents.len else [cur])
 
+    def process_file(self, filename):
+        """Process a parsed org file and save into database."""
+        # Scan text before first heading
+        # to find tag hierarchies.
+        # Tag hierarchies are preserved across all files
+        # to keep the program simple
+        org_file = load(filename)
+        if not self.db:
+            raise ValueError('No database connection')
+        for line in str(org_file):
+            m = regex.match(
+                r"^#\+tags:\s+[\[{]\s+([A-Za-z_@]+)\s+:\s+(?:([A-Za-z_@]+)\s+)+[\]}]",
+                line,
+                regex.I)
+            if m:
+                parent_tag = m.groups()[0]
+                self.tag_hierarchy[parent_tag] = m.captures(2)
+        self.build_tag_ancestors()
 
-def process_file(org_file):
-    """Process a parsed org file and save into database."""
-    # TODO tags tree; see tag_{hierarchy,parents}
-    # scan text before first heading
-    for line in str(org_file):
-        m = regex.match(
-            r"^#\+tags:\s+[\[{]\s+([A-Za-z_@]+)\s+:\s+(?:([A-Za-z_@]+)\s+)+[\]}]",
-            line,
-            regex.I)
-        if m:
-            parent_tag = m.groups()[0]
-            tag_hierarchy[parent_tag] = m.captures(2)
-    build_tag_ancestors()
+        # recursively scan for headlines with clocks
+        # and save them into database
+        cursor = self.db.cursor()
+        # TODO
+        raise NotImplementedError
+
+    def process_files(self, filenames=[], dirs=[]):
+        """Process files and directories (recursively)."""
+        if not (filenames or dirs):
+            raise ValueError('No input (filename / dirs) specified')
+        for d in dirs:
+            for root, _, files in os.walk(d):
+                for f in files:
+                    if f.endswith(".org"):
+                        filenames.append(os.path.join(root, f))
+
+        for filename in filenames:
+            self.process_file(filename)
